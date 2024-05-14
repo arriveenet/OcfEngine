f3c36de703af4d4ed3d1a316e124edcb6ab29beb
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2024-05-14 21:35:26 +0900

    エンジンをライブラリに変更

---
+++ tests/cpp-tests/main.cpp
@@ -0,0 +1,20 @@
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+#include "platform/Application.h"
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "OcfEngine.lib")
+
+using namespace ocf;
+
+int main(int argc, char** argv)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	int result = EXIT_SUCCESS;
+
+	Applicaiton* app = Applicaiton::getInstance();
+	result = app->run();
+
+	return result;
+}
