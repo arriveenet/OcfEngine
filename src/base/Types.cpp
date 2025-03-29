5b46da246adc7a703d914f1f0d353fb9125b1f8d
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2025-03-29 21:19:31 +0900

    `Color3b`と`Color4f`に新しい色定数を追加
    `Types.cpp`と`Types.h`において、`Color3b`と`Color4f`の新しい色定数が追加されました。追加された色は、`GRAY`、`YELLOW`、`ORANGE`、`MAGENTA`です。

--- src/base/Types.cpp
+++ src/base/Types.cpp
@@ -4,14 +4,22 @@ NS_OCF_BEGIN
 
 const glm::u8vec3 Color3b::BLACK(0, 0, 0);
 const glm::u8vec3 Color3b::WHITE(255, 255, 255);
+const glm::u8vec3 Color3b::GRAY(166, 166, 166);
 const glm::u8vec3 Color3b::RED(255, 0, 0);
 const glm::u8vec3 Color3b::GREEN(0, 255, 0);
 const glm::u8vec3 Color3b::BLUE(0, 0, 255);
+const glm::u8vec3 Color3b::YELLOW(255, 255, 0);
+const glm::u8vec3 Color3b::ORANGE(255, 127, 0);
+const glm::u8vec3 Color3b::MAGENTA(255, 0, 255);
 
 const glm::vec4 Color4f::BLACK(0.0f, 0.0f, 0.0f, 1.0f);
 const glm::vec4 Color4f::WHITE(1.0f, 1.0f, 1.0f, 1.0f);
+const glm::vec4 Color4f::GRAY(0.65f, 0.65f, 0.65f, 1.0f);
 const glm::vec4 Color4f::RED(1.0f, 0.0f, 0.0f, 1.0f);
 const glm::vec4 Color4f::GREEN(0.0f, 1.0f, 0.0f, 1.0f);
 const glm::vec4 Color4f::BLUE(0.0f, 0.0f, 1.0f, 1.0f);
+const glm::vec4 Color4f::YELLOW(1.0f, 1.0f, 0.0f, 1.0f);
+const glm::vec4 Color4f::ORANGE(1.0f, 0.5f, 0.0f, 1.0f);
+const glm::vec4 Color4f::MAGENTA(1.0f, 0.0f, 1.0f, 1.0f);
 
 NS_OCF_END
